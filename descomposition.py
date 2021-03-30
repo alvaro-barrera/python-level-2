25cee9893f738c84b2092844eb6526098f3c52ad
Alvaro Barrera <alvaro_bv@hotmail.com>
2021-03-30 10:37:46 -0500

    Abstracción

--- descomposition.py
+++ descomposition.py
@@ -13,6 +13,12 @@ class Car:
         else:
             self._engine.fuel_inyection(3)
 
+    def maintenance(self):
+        self._check_fuel()
+
+    def _check_fuel(self):
+        print('Revisión de combustible')
+
 
 class Engine:
     def __init__(self, cylindres, type='gasoline'):
@@ -25,8 +31,9 @@ class Engine:
 
 
 def run():
-    pass
+    car = Car('Toyota 4Runner.', 'Toyota', 'Plomo')
+    car.maintenance()
+
 
 if __name__ == '__main__':
     run()
-
